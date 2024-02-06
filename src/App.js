1c3311885af2862c9956d0aa9ca74ad94c11abd2
AlfiyaSainudeen <154522933+AlfiyaSainudeen@users.noreply.github.com>
2024-02-06 19:38:24 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Nm from './noti';
+import Not from './Nottification';
+import CommunicationModule from './Nottification';
+import MessageBox from './msg';
+
+
+function App() {
+  return (
+    <div className="App">
+   {/* <Not/> */}
+   {/* <CommunicationModule/> */}
+   <Nm/>
+   {/* <MessageBox/> */}
+   {/* <Email/> */}
+    </div>
+  );
+}
+
+export default App;
